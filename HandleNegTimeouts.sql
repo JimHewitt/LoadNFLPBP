c334f7315bee34be8213d48131bd93ef4fb36721
Jim Hewitt <jhewitt2@new.rr.com>
2026-02-11 20:20:11 -0600

    Add files via upload
    Fix for negative timeout values.

---
+++ HandleNegTimeouts.sql
@@ -0,0 +1,15 @@
+ALTER TABLE PBPRaw
+ALTER COLUMN home_timeouts_remaining smallint
+GO
+
+ALTER TABLE PBPRaw
+ALTER COLUMN away_timeouts_remaining smallint
+GO
+
+ALTER TABLE PBPRaw
+ALTER COLUMN posteam_timeouts_remaining smallint
+GO
+
+ALTER TABLE PBPRaw
+ALTER COLUMN defteam_timeouts_remaining smallint
+GO
